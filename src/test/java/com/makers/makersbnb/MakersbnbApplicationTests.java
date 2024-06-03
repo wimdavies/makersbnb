77b802155ccb3466fd20514970938bd0fa768f4d
wimdavies <william.johnston.davies@gmail.com>
2024-06-03 10:06:13 +0100

    Project setup

---
+++ src/test/java/com/makers/makersbnb/MakersbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makers.makersbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakersbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
