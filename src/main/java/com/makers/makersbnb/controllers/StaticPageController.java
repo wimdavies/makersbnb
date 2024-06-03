4588894e51886cd4b22193cd6a8bca91e1276255
wimdavies <william.johnston.davies@gmail.com>
2024-06-03 11:12:01 +0100

    Change / to serve template

--- src/main/java/com/makers/makersbnb/controllers/StaticPageController.java
+++ src/main/java/com/makers/makersbnb/controllers/StaticPageController.java
@@ -2,12 +2,13 @@ package com.makers.makersbnb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class StaticPageController {
     @GetMapping("/")
-    public String landingPage() {
-        return "Welcome to MakersBnB!";
+    public ModelAndView landingPage() {
+        return new ModelAndView("/LandingPage");
     }
 
     @GetMapping("/contactus")
