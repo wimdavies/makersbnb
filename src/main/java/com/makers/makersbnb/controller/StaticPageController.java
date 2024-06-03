86e688c868e379fa1065929f1b6cb7c40d82ab51
wimdavies <william.johnston.davies@gmail.com>
2024-06-03 16:10:03 +0100

    Field inject repository to controller, use actual space count in view

--- src/main/java/com/makers/makersbnb/controller/StaticPageController.java
+++ src/main/java/com/makers/makersbnb/controller/StaticPageController.java
@@ -1,15 +1,20 @@
 package com.makers.makersbnb.controller;
 
+import com.makers.makersbnb.repository.SpaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class StaticPageController {
+    @Autowired
+    private SpaceRepository spaceRepository;
+
     @GetMapping("/")
     public ModelAndView landingPage() {
         ModelAndView modelAndView = new ModelAndView("/LandingPage");
-        Integer nSpaces = 100;
+        long nSpaces = spaceRepository.count();
         modelAndView.addObject("nSpaces", nSpaces);
         Integer nBookings = 123;
         modelAndView.addObject("nBookings", nBookings);
