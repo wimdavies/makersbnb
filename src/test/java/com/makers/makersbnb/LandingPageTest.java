3bfb4bd59e64aee5f9ac385e0962f409c1727930
wimdavies <william.johnston.davies@gmail.com>
2024-06-03 10:59:50 +0100

    Add tests for /contactus and /team

--- src/test/java/com/makers/makersbnb/LandingPageTest.java
+++ src/test/java/com/makers/makersbnb/LandingPageTest.java
@@ -1,2 +1,56 @@
-package com.makers.makersbnb;public class LandingPageTest {
+package com.makers.makersbnb;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+class LandingPageTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    public void usersAreWelcomedToTheApp() {
+        page.navigate("localhost:8080");
+        Locator pageBody = page.locator("body");
+        assertThat(pageBody).containsText("Welcome to MakersBnB!");
+    }
+
+    @Test
+    public void usersCanSeeContactEmailAddress() {
+        page.navigate("localhost:8080/contactus");
+        Locator pageBody = page.locator("body");
+        assertThat(pageBody).containsText("email@website.com");
+    }
+
+    @Test
+    public void usersCanSeeTeamList() {
+        page.navigate("localhost:8080/team");
+        Locator pageBody = page.locator("body");
+        assertThat(pageBody).containsText("Toby");
+    }
 }
