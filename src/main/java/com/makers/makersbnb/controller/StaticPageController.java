8db0265223b29d8acff21ae096a737d15677ce66
wimdavies <william.johnston.davies@gmail.com>
2024-06-04 17:53:54 +0100

    Add logout link to nav, add named greeting to home if user logged in

--- src/main/java/com/makers/makersbnb/controller/StaticPageController.java
+++ src/main/java/com/makers/makersbnb/controller/StaticPageController.java
@@ -2,6 +2,8 @@ package com.makers.makersbnb.controller;
 
 import com.makers.makersbnb.repository.SpaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,8 +14,12 @@ public class StaticPageController {
     private SpaceRepository spaceRepository;
 
     @GetMapping("/")
-    public ModelAndView landingPage() {
+    public ModelAndView landingPage(@AuthenticationPrincipal OAuth2User principal) {
         ModelAndView modelAndView = new ModelAndView("/LandingPage");
+        if (principal != null) {
+            String username = principal.getAttribute("name");
+            modelAndView.addObject("username", username);
+        }
         long nSpaces = spaceRepository.count();
         modelAndView.addObject("nSpaces", nSpaces);
         Integer nBookings = 123;
