8a85eee1f227e73ccf6fa074000602cb526f945e
wimdavies <william.johnston.davies@gmail.com>
2024-06-06 14:23:51 +0100

    Add custom AuthenticationSuccessHandler to save new oauth2logins to the db as Users. Redirect only to / on login success

--- src/main/java/com/makers/makersbnb/config/SecurityConfiguration.java
+++ src/main/java/com/makers/makersbnb/config/SecurityConfiguration.java
@@ -1,21 +1,64 @@
 package com.makers.makersbnb.config;
 
+import com.makers.makersbnb.model.User;
+import com.makers.makersbnb.repository.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    private final UserRepository userRepository;
+
+    public SecurityConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests( request -> {
             request.requestMatchers("/", "/contactus", "/termsandconditions", "/team").permitAll();
             request.anyRequest().authenticated();
         })
-                .oauth2Login(withDefaults()).build();
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .successHandler(authenticationSuccessHandler())
+                                .defaultSuccessUrl("/", true)
+                )
+                .build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+                String name = oAuth2User.getAttribute("name");
+                Integer githubId = oAuth2User.getAttribute("id");
+
+                User foundUser = userRepository.findByGithubId(githubId);
+                if (foundUser == null) {
+                    User user = new User();
+                    user.setName(name);
+                    user.setGithubId(githubId);
+                    userRepository.save(user);
+                }
+            }
+        };
     }
 }
